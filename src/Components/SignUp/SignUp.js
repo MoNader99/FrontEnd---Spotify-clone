d431ea3700ae6f203fe2e0ee167ea239e3cdc7ba
MohammedNader99 <mnader93@yahoo.com>
2020-05-10 21:28:07 +0200

    adding signUp component (design only) no verification

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -2,324 +2,40 @@ import React ,{ Component} from 'react';
 import './SignUp.css';
 
 
-
-const emailFormat = RegExp(
-  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-);
-
-
 export class SignUp extends Component{
-  constructor(props){
-    super(props);
+  render() 
+	{
+    return(
+      <div className="Sign-Up">
 
-    this.state= {
-        
-      email: null,
-       
-      password: null,
-        
-      username: null,
-       
-      day: null,
-       
-      month: null,
-        
-      year: null,
-        
-      gender: null,
-        
-      accountType: null,
-       
-      alreadyExists: "",
-      
-      errorMessages: {
-       
-        email: " ",
-        
-        password: " ",
-       
-        username: " ",
-       
-        day: " ",
-       
-        month: " ",
-       
-        year: " ",
-       
-        gender: " ",
-      
-        accountType: " ",
-      }
-    };
-  }
-
-  formValidity = () => {
-    let valid = true;
-    
-
-    Object.values(this.state.errorMessages).forEach(val => {
-      if (val !== null && val.length === 0 ) {
-        valid = true;
-      }
-      else if ( val === null || val !== null && val.length > 0 ) {
-        valid = false;
-        console.log ("value is "+val +"length is" +val.length);
-      }
-
-    });
-
-    return valid;
-  };
-    
-  handleSubmit =e => {
-    e.preventDefault();
-    let errorMessages= { ...this.state.errorMessages };
-    const {email,  password, username, day, month, year, gender, accountType} = this.state;
-  
-    var isPremium= false;
-    console.log(this.state);
-
-    if( accountType=== "Premium Account") {
-      isPremium= true;
-    }
-    if (this.formValidity(this.state)) {
-  
-      // const url = BASEURL+"users/signup";
-      // console.log(url);
-      // const requestOptions = {
-      //   method: 'POST',  
-      //   headers: { 'Content-Type': 'application/json' },
-      //   body: JSON.stringify({
-      //     userName : username,
-      //     email: email,
-      //     isPremium: false,
-      //     password: password,
-      //     day: day,
-      //     month: month,
-      //     year: year,
-      //     gender: gender
-      //   })
-      //   }
-      //   console.log(requestOptions)
-      //   fetch(url, requestOptions)
-      //   .then(response => response.text())
-      //   .then(data => {
-      //     console.log(data);
-      //     if(data == "User added Successfully as inActive. Waiting for Email Confirmation "){
-      //       this.props.history.replace("/signup/emailsent/");
-      //     }
-      //     else if(data == "UserName and/or email already exist"){
-      //       var err= document.getElementsByClassName("already-exists");
-      //       err[0].innerHTML= "UserName and/or email already exist";
-      //       document.getElementById("signup-form").reset();
-      //       window.location.reload(true);
-      //     }
-      //   })
-      //   .catch((error)=> {console.log(error)});
-      }
-    
-    else {
-      if ( email === null )
-        errorMessages.email = "Please enter your email.";
-
-        if ( password === null )
-        errorMessages.password = "Please enter your password.";
-
-        if ( username === null )
-        errorMessages.username = "What should we call you?";  
-        
-        if ( day === null )
-        errorMessages.day = "Please choose a day."; 
-
-        if ( month === null )
-        errorMessages.month = "Please choose a month."; 
-
-        if ( year === null )
-        errorMessages.year = "Please choose a year."; 
-
-        if ( gender === null )
-        errorMessages.gender = "Please select your gender."; 
-
-        if ( accountType === null )
-        errorMessages.accountType = "What type of account do you like?"; 
-      
-        this.setState( { errorMessages }, () => console.log(this.state) );
-        console.log("ERROR");
-    }
-
-
-    Object.entries(errorMessages).forEach (pair => {
-      if ( pair[1] != null && pair[0] != "gender" && pair[1].length > 1) {
-      
-        var fieldToBeChanged = document.getElementsByName( pair[0] );
-        fieldToBeChanged[0].style.borderColor= "#bd3200";
-        console.log (pair);
-      }
-    });
-
-  };
-
-  handleChange =e => {
-       
-    const { name, value }= e.target;
-      
-    let errorMessages= { ...this.state.errorMessages };  
-    this.setState({ [name]: value}, () => console.log(this.state) );
-
-    switch (name) {
-
-      case "email":
-        if ( value.length === 0 ) {
-          errorMessages.email = "Please enter your email.";
-
-        }
-        else if ( !emailFormat.test(value) ) {
-          errorMessages.email = "The email address you entered is invalid.";
-        }
-        else {
-          errorMessages.email = "";
-        }
-        break;
-
-      case "password":
-        if ( value.length === 0 ) {
-          errorMessages.password = "Please enter your password.";
-        }
-        else if (value.length < 6) {
-          errorMessages.password = "The password you entered is too short.";
-        }
-        else {
-          errorMessages.password = "";
-        }
-        break;
-      
-      case "username":
-        if ( value.length === 0 ) {
-          errorMessages.username = "What should we call you?";
-        }
-        else {
-          errorMessages.username = "";
-        }
-        break;
-      
-      case "day":
-        if ( value.length === 0 ) {
-          errorMessages.day = "Please choose a day.";
-        }
-        else if ( value > 31 || value < 1 ) {
-          errorMessages.day = "Please choose a valid day of the month.";
-        }
-        else {
-          errorMessages.day = "";
-        }
-        break;
-
-      case "month":
-        if ( value.length === 0 ) {
-          errorMessages.month = "Please choose a month.";
-        }
-        else {
-          errorMessages.month = "";
-        }
-        break;
-
-      case "year":
-        if ( value.length === 0 ) {
-          errorMessages.year = "Please choose a year.";
-        }
-        else if ( value > 2020 || value < 1990 ) {
-          errorMessages.year = "Please choose a valid year (1990-2020).";
-        }
-        else {
-          errorMessages.year = "";
-        }
-        break;
-
-      case "gender":
-        if ( value.length === 0 ) {
-          errorMessages.gender = "Please select your gender.";
-        }
-        else {
-          errorMessages.gender = "";
-        }
-        break;  
-
-      case "accountType":
-        if ( value.length === 0 ) {
-          errorMessages.accountType = "What type of account do you like?";
-        }
-        else {
-          errorMessages.accountType = "";
-        }
-        break;              
-
-      default:
-        break;
-    }
-
-    this.setState({ errorMessages}, () => console.log(this.state) );
-    
-    // Check which input fields (except gender) have errors to give them a red border
-    // pair: [name, value]
-    Object.entries(errorMessages).forEach (pair => {
-      if ( pair[1] != null && pair[0] != "gender" && pair[1].length > 1) {
-      /**array of inputs to be given a style
-       * @memberof SignUp
-       * @type {Array<elements>}
-       */
-        var fieldToBeChanged = document.getElementsByName( pair[0] );
-        if(fieldToBeChanged[0]) {
-        fieldToBeChanged[0].style.borderColor= "#bd3200";}
-        // console.log (pair);
-      }
-      else if ( pair[1] != null && pair[0] != "gender" && pair[1].length === 0) {
-        var fieldToBeChanged = document.getElementsByName( pair[0] );
-        if(fieldToBeChanged[0]) {
-        fieldToBeChanged[0].style.borderColor= "#ced4da";}
-        // console.log (pair);
-      }
-    })
-  };
-
-
-
-  render() {
-    const { errorMessages } = this.state;
-    return (
-      <div className="signup-body">
-          <div id= "logo" className="container-fluid border-bottom p-md-4 p-s-2">
-            <a href="/" ></a> 
+          <div className="container-fluid border-bottom content mb-5">
+              <a href="/" className="Image container"></a> 
           </div>
 
-          <div className="container text-left signup-box">
-            <div>
-              <h2 className="already-exists"> {this.state.alreadyExists} </h2>
-              <h2 > Sign up with your email address </h2>
-            </div>
+          <div className="mb-3">
+          <a className="sign-with-facebook btn rounded-pill text-center">SIGN UP WITH FACEBOOK</a>
+          </div>
 
-            <form  id="signup-form" noValidate>
+          <p className="or">or</p>
+          <h2>Sign up with your email address</h2>
 
-              <input 
-              name= "email" id="email-input" className="form-control input-field" type="email" placeholder="Email" onChange={this.handleChange}/> 
-              <p id="empty-email" className="empty-input"> {errorMessages.email}</p>
+          <div className="container ">
+            <form class="needs-validation" novalidate>
+                <div class="form-group d-flex justify-content-center">
+                  <input type="email" className="form-control" placeholder="Email"></input>
+                </div>
+                <div class="form-group d-flex justify-content-center">
+                  <input type="email" className="form-control" placeholder="Confirm Email"></input>
+                </div>
+                <div class="form-group d-flex justify-content-center">
+                  <input type="password" className="form-control" placeholder="Password"></input>
+                </div>
+                <div class="form-group d-flex justify-content-center">
+                  <input type="text" className="form-control" placeholder="What should we call you ?"></input>
+                </div>
 
-              <input name="password" className="form-control input-field" type="Password" placeholder="Password" onChange={this.handleChange}/> 
-              <p id="empty-password" className="empty-input"> {errorMessages.password} </p>
-
-              <input name="username" className="form-control input-field" type="text" placeholder="What should we call you?" onChange={this.handleChange}/> 
-              <p id="empty-userame" className="empty-input pb-2"> {errorMessages.username}</p>
-                  
-              <div className="form-group">
-
-                <label htmlFor="birth-date"> Date of birth </label>
-                <div className="row" id="birth-date">
-
-                  <div className="col-3">
-                    <input name="day" className=" form-control birth-date-signup" type="number" placeholder="Day" max="31" min="1" maxLength="2" onChange={this.handleChange}/>
-                  </div>
-
-                  <div className="col-6">
-                    <select name="month" className="form-control birth-date-signup dropdown" onChange={this.handleChange}> 
+                <div className="d-flex justify-content-center" >
+                <select class="custom-select months " placeholder="What should we call you ?">
                       <option value=""> Month </option>
                       <option value="01"> January </option>
                       <option value="02"> February </option>
@@ -333,63 +49,39 @@ export class SignUp extends Component{
                       <option value="10"> October </option>
                       <option value="11"> November </option>
                       <option value="12"> December </option>
-                    </select>
+                </select>
+                <input type="text" className="form-control days" placeholder="Day"></input>
+                <input type="text" className="form-control year" placeholder="year"></input>
+                </div>
+
+                <div className="mt-3 ">
+                  <div class="custom-control custom-radio custom-control-inline">
+                    <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input"></input>
+                    <label class="custom-control-label" for="customRadioInline1">Male</label>
                   </div>
-
-                  <div className="col-3 ">
-                    <input name="year" className="form-control birth-date-signup" type="number" placeholder="Year" max="1999" min="1900" maxLength="4" onChange={this.handleChange}/>
+                  <div class="custom-control custom-radio custom-control-inline">
+                    <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input"></input>
+                    <label class="custom-control-label" for="customRadioInline2">Female</label>
                   </div>
-
-                </div>
-              </div>
-
-              <p id="empty-day" className="empty-input"> {errorMessages.day}</p>
-              <p id="empty-month" className="empty-input"> {errorMessages.month}</p>
-              <p id="empty-year" className="empty-input pb-2"> {errorMessages.year}</p>
-
-              <div className="form-group">
-
-                <label htmlFor="gender-options"> Gender</label>
-                <div className="m-2" id="gender-options">
-                    <input name="gender" type="radio" value="M" onChange={this.handleChange}/> Male   
-                    <input name="gender" type="radio" value="F" className="ml-3" onChange={this.handleChange}/> Female
+                  <div class="custom-control custom-radio custom-control-inline">
+                    <input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input"></input>
+                    <label class="custom-control-label" for="customRadioInline3">Non-Binary</label>
+                  </div>
                 </div>
 
-              </div>
-          
-              <p id="empty-gender" className="empty-input pb-2"> {errorMessages.gender}</p>
-
-              <div className="form-group">
-
-                <label htmlFor="account-type">What kind of account do you like?</label>
-                <div >
-                  <select className="form-control birth-date-signup dropdown" name="accountType" onChange={this.handleChange} > 
-                    <option value=""> Choose from here </option>
-                    <option value="Regular Account"> Regular Account</option>
-                    <option value="Premium Account"> Premium Account</option>
-                    <option value="Artist Account">Artist Account</option>
-                  </select>
+                <div className="terms-and-conditions mt-3 ">
+                  <p className="mb-3">By clicking on Sign up, you agree to Spotify's<a className="green" href="/"> Terms and Conditions of Use</a>.</p>
+                  <p className="mb-3">To learn more about how Spotify collects, uses, shares and protects your personal data please read Spotify's<a className="green" href=""> Privacy Policy</a>.</p>
                 </div>
 
-                <p id="empty-account-type" className="empty-input pt-2"> {errorMessages.accountType}</p>
-
-              </div>
-
-              <button type="submit" id="signup-button" className="btn btn-success rounded-pill text-center d-flex justify-content-center" onClick={this.handleSubmit}>
-              SIGN UP 
-              </button>  
-
+                <a type="submit" className="submit btn rounded-pill text-center mb-3">SIGN UP</a>
+                <p style={{'font-size' :'0.75em', 'font-weight': '400'}}>Already have an account? <a className="green" href="/logIn">Log in</a></p>
             </form>
-
-            <p> Already have an account? <a href="/logIn"> Log in </a> </p>
-
           </div>
 
       </div>
     );
   }
-
-  
 };
 
 export default  SignUp;
