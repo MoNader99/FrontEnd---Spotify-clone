41bfc1a18dd649d58b5a6ff06d1da06b3fce3988
hoda257 <hodahossam257@gmail.com>
2020-05-11 17:58:31 +0200

    Adding routes for new components

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import {BrowserRouter as Router,
 import welcomePage from "./Containers/welcomePage/welcomePage";
 import SignUp from "./Components/SignUp/SignUp";
 import Search from './Components/Search/Search';
+import Login from './Components/Login/Login';
+import PlaylistPage from './Components/PlaylistsComponent/PlaylistPage'
+import { AlbumPage } from './Components/AlbumComponent/AlbumPage';
+import LikedSongs from './Components/LikedSongs/LikedSongs';
 
 function App() {
   return (
@@ -19,7 +23,11 @@ function App() {
 
           <Route exact path="/" component={welcomePage}/>
           <Route  exact path="/signup/" component={SignUp}/>
-          <Route  exact path="/search/" component={Search}/>
+          <Route  exact path="/login/" component={Login}/>
+          <Route  exact path="/webplayer/search/" component={Search}/>
+          <Route  exact path="/webplayer/playlist/" component={PlaylistPage}/>
+          <Route  exact path="/webplayer/album/" component={AlbumPage}/>
+          <Route  exact path="/webplayer/likedsongs/" component={LikedSongs}/>
           {/* TODO: add routes to the other pages */}
         </Switch>
       </Router>
