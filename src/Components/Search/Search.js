566e6da7d9efa704d4fea34c4c9468e376e164ae
MohammedNader99 <mnader93@yahoo.com>
2020-05-13 03:16:22 +0200

    Dynamic search finished with hard coded data , not responsive don't panic

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -121,43 +121,193 @@ class Search extends Component {
                     <div className="component-content ">
                             <p className=" browse">Top results</p>
                             <div className="row">
-                            <div className="col-3 text-white bg-info ">
-                                ARTIST CARD
+                            <div className="col-3 p-0">
+                                <div class="top-result-card">
+                                    <img src="https://ph-files.imgix.net/cbbf111b-fccf-48a7-8505-bedc7b5d5272?auto=format" className="image d-flex align-items-start" ></img>
+                                    <h3 className="text-white d-flex align-items-start mt-3 font-weight-bold">Playlist Name</h3>
+                                    <div>
+                                        <p className="text-white d-flex align-items-start mt-1 font-weight-bold">By User Name</p>
+                                    </div>
+                                </div>
+
                             </div>
-                            <div className="col-9 bg-warning">
-                                    Songs
+                            <div className="col-9 ">
+                            <div className="songs">
+                                <div className="row">
+                                <div className="col-xl-1 col-md-1 col-1 col-2">
+                                    <a>
+                                        <img src='https://ph-files.imgix.net/cbbf111b-fccf-48a7-8505-bedc7b5d5272?auto=format' className="p-1 image"></img>
+                                    </a>
+                                </div>
+                                <div className="col-xl-8 col-md-6 col-sm-6 col-6 mt-3 d-flex align-items-start">
+                                <ul className="list-unstyled ">
+                                    <li className="d-flex align-items-start">Song name</li>
+                                    <li className="song-info"><a href='/webplayer/artistprofile/'>Artist Name</a> <span className="font-weight-bold">.</span> <a href='/webplayer/album'> Album Name </a></li>
+                                </ul>
+                                </div>
+                                <div className="col-xl-2 col-md-4 col-sm-4 col-4 d-flex justify-content-end">
+                                    
+                                </div>
+                                </div>
+                            </div>
+                            <div className="songs">
+                                <div className="row">
+                                <div className="col-xl-1 col-md-1 col-1 col-2">
+                                    <a>
+                                        <img src='https://ph-files.imgix.net/cbbf111b-fccf-48a7-8505-bedc7b5d5272?auto=format' className="p-1 image"></img>
+                                    </a>
+                                </div>
+                                <div className="col-xl-8 col-md-6 col-sm-6 col-6 mt-3 d-flex align-items-start">
+                                <ul className="list-unstyled ">
+                                    <li className="d-flex align-items-start">Song name</li>
+                                    <li className="song-info"><a href='/webplayer/artistprofile/'>Artist Name</a> <span className="font-weight-bold">.</span> <a href='/webplayer/album'> Album Name </a></li>
+                                </ul>
+                                </div>
+                                <div className="col-xl-2 col-md-4 col-sm-4 col-4 d-flex justify-content-end">
+                                    
+                                </div>
+                                </div>
+                            </div>
+                            <div className="songs">
+                                <div className="row">
+                                <div className="col-xl-1 col-md-1 col-1 col-2">
+                                    <a>
+                                        <img src='https://ph-files.imgix.net/cbbf111b-fccf-48a7-8505-bedc7b5d5272?auto=format' className="p-1 image"></img>
+                                    </a>
+                                </div>
+                                <div className="col-xl-8 col-md-6 col-sm-6 col-6 mt-3 d-flex align-items-start">
+                                <ul className="list-unstyled ">
+                                    <li className="d-flex align-items-start">Song name</li>
+                                    <li className="song-info"><a href='/webplayer/artistprofile/'>Artist Name</a> <span className="font-weight-bold">.</span> <a href='/webplayer/album'> Album Name </a></li>
+                                </ul>
+                                </div>
+                                <div className="col-xl-2 col-md-4 col-sm-4 col-4 d-flex justify-content-end">
+                                    
+                                </div>
+                                </div>
+                            </div>
                             </div>
                             </div>     
                     </div>
+
+
                     <div className="component-content ">
                         <h2 className=" browse" >Artists</h2>
                             <div className="row">
-                            <div className="col-12 text-white bg-info ">
-                                ARTIST CARDS
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="artist-card"  >
+                                    <img src="https://vanyaland.com/wp-content/uploads/2017/07/KendrickLamar_PublicityImage.jpg" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">Artist Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >artist</h5>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="artist-card"  >
+                                    <img src="https://www.grammy.com/sites/com/files/styles/news_detail_header/public/katy_perry_020419.jpg?itok=1eyJ68SB" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">Artist Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >artist</h5>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="artist-card"  >
+                                    <img src="https://etbilarabi.com/sites/default/files/styles/article_landing/public/2020-03/BAFBDC61-35D2-4A4E-A9F6-4C41F1B47403.jpeg?itok=zNDV0gxy" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">Artist Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >artist</h5>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="artist-card"  >
+                                    <img src="https://www.nme.com/wp-content/uploads/2017/05/lana_del_rey_new_song_coachella_2017_1000-696x442.jpg" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">Artist Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >artist</h5>
+                                </a>
                             </div>
                             </div>     
                     </div>
+
+
                     <div className="component-content ">
                         <h2 className=" browse" >Albums</h2>
                             <div className="row">
-                            <div className="col-12 text-white bg-success ">
-                                Albums CARDS
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="albums-playlists-card"  >
+                                    <img src="https://pro2-bar-s3-cdn-cf6.myportfolio.com/0d5a2028d7a7f8febf676a97648ea300/05851b218f8801ca2b106119_rw_1920.jpg?h=3e70946439d6e6326c1bd3efc5d20e51" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">Album Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >artist name</h5>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="albums-playlists-card"  >
+                                    <img src="https://stcmlbpp1316.files.wordpress.com/2016/09/computergraphics-album-covers-2014-15.jpg" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">Album Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >artist name</h5>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="albums-playlists-card"  >
+                                    <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_68585523.jpg?auto=format&q=60&fit=max&w=930" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">Album Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >artist name</h5>
+                                </a>
                             </div>
                             </div>     
                     </div>
+
+
                     <div className="component-content ">
                         <h2 className=" browse" >playlists</h2>
                             <div className="row">
-                            <div className="col-12 text-white bg-danger ">
-                                PLAYLISTS CARDS
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="albums-playlists-card"  >
+                                    <img src="https://i.ibb.co/MDQvq27/Empty-playlist.jpg" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">playlist Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >By user name</h5>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="albums-playlists-card"  >
+                                    <img src="https://i.ibb.co/MDQvq27/Empty-playlist.jpg" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">playlist Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >By user name</h5>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="albums-playlists-card"  >
+                                    <img src="https://i.ibb.co/MDQvq27/Empty-playlist.jpg" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">playlist Name</h3>
+                                    <h5 className=" text-white d-flex align-items-start" >By user name</h5>
+                                </a>
                             </div>
                             </div>     
                     </div>
+
+
                     <div className="component-content ">
                         <h2 className=" browse" >Profiles</h2>
                             <div className="row">
-                            <div className="col-12 text-white bg-warning">
-                                PROFILES CARDS
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="artist-card"  >
+                                    <img src="https://res.cloudinary.com/dtbudl0yx/image/fetch/w_2000,f_auto,q_auto,c_fit/https://adamtheautomator.com/content/images/size/w2000/2019/10/user-1633249_1280.png" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">User Name</h3>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="artist-card"  >
+                                    <img src="https://res.cloudinary.com/dtbudl0yx/image/fetch/w_2000,f_auto,q_auto,c_fit/https://adamtheautomator.com/content/images/size/w2000/2019/10/user-1633249_1280.png" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">User Name</h3>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="artist-card"  >
+                                    <img src="https://res.cloudinary.com/dtbudl0yx/image/fetch/w_2000,f_auto,q_auto,c_fit/https://adamtheautomator.com/content/images/size/w2000/2019/10/user-1633249_1280.png" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">User Name</h3>
+                                </a>
+                            </div>
+                            <div className="col-xl-1 artist-card ">
+                                <a href="/webplayer/playlist" class="artist-card"  >
+                                    <img src="https://res.cloudinary.com/dtbudl0yx/image/fetch/w_2000,f_auto,q_auto,c_fit/https://adamtheautomator.com/content/images/size/w2000/2019/10/user-1633249_1280.png" class="img-style" alt=""></img>
+                                    <h3 class="artist-info-style text-white d-flex align-items-start mt-3 ">User Name</h3>
+                                </a>
                             </div>
                             </div>     
                     </div>
